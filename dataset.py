330d7454c678254602ee3e958d0a84cee4b93cd6
oxyde <s.gibard@gmail.com>
2021-04-25 18:09:03 +0200

    Implementation of Dataset object - training_set - validation_set - training_target - validation_target

--- dataset.py
+++ dataset.py
@@ -14,5 +14,9 @@ class Dataset:
             raise(ValueError("Error: Dataset - Split value must be less than 1.0."))
 
     def __create_training_and_validation_set(self, dataset: pd.DataFrame, target: pd.DataFrame, split: int):
-        self.training_set, self.validation_set, self.training_target, self.validation_target = train_test_split(dataset, target, train_size=split, test_size=1-split,
-                                                              random_state=0)
+        self.training_set, self.validation_set, self.training_target, self.validation_target = train_test_split(
+            dataset,
+            target,
+            train_size=split,
+            test_size=1-split,
+            random_state=0)
