ee7779a03c6933e9447d6d64c94e75ac10a2da10
oxyde <s.gibard@gmail.com>
2021-05-14 01:21:31 +0200

    from model_generator.py : move generate_submission_file function to the Test_set object functionnalities

--- test_set.py
+++ test_set.py
@@ -4,5 +4,10 @@ import pandas as pd
 
 class Test_set(Dataset):
 
-    def __init__(self, testset: pd.DataFrame):
-        self.dataset = testset
+    def __init__(self, test_set: pd.DataFrame):
+        self.dataset = test_set
+
+    def generate_submission_file(self, prediction_test):
+        output = pd.DataFrame({'Id': self.dataset.index,
+                               'SalePrice': prediction_test})
+        output.to_csv('submission.csv', index=False)
