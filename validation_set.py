ee2acb34ef549ee42cfe5b0d8ab7598e014875fb
oxyde <s.gibard@gmail.com>
2021-05-14 01:17:27 +0200

    Add ModelEvaluator functionnalities to the Validation_set object

--- validation_set.py
+++ validation_set.py
@@ -1,5 +1,7 @@
 from dataset import Dataset
+from model_evaluator import ModelEvaluator
 import pandas as pd
+import numpy as np
 
 class Validation_set(Dataset):
 
@@ -7,3 +9,6 @@ class Validation_set(Dataset):
         super().__init__(dataset, target, split)
         self.dataset = self._validation_set
         self.target = self._validation_target
+
+    def evaluate_model_with_mean_absolute_error(self, prediction: np.ndarray):
+        return ModelEvaluator(self.dataset, prediction).evaluate_mean_absolute_error()
