be9462c2ee06e5da4ef3c4e1967a1f1d633da592
oxyde <s.gibard@gmail.com>
2021-05-15 17:06:17 +0200

    Creating base to work on

--- deal_with_categ_var.py
+++ deal_with_categ_var.py
@@ -1,14 +1,20 @@
 import pandas as pd
 
+
 class DealWithCategVar:
 
-    def __init__(self, X: pd.DataFrame, unique_var_limiter: int):
+    def __init__(self, X: pd.DataFrame, unique_var_limiter=None):
         self.X = X
         self.unique_var_limiter = unique_var_limiter
-        self.col_with_categ_data = []
+
+        if unique_var_limiter == None:
+            self.__create_list_of_cols_with_categ_data()
+        else:
+            self.__create_list_of_cols_with_categ_data_according_to_limiter()
 
     def __create_list_of_cols_with_categ_data(self):
         self.col_with_categ_data = [col for col in self.X.columns if self.X[col].dtypes == 'object']
 
     def __create_list_of_cols_with_categ_data_according_to_limiter(self):
-        self.col_with_categ_data = [col for col in self.X.columns if len(self.X[col].unique()) > self.unique_var_limiter]
+        self.col_with_categ_data = [col for col in self.X.columns if
+                                    len(self.X[col].unique()) > self.unique_var_limiter]
--- house_price_categ_var.py
+++ house_price_categ_var.py
@@ -1,19 +1,14 @@
-from dataset_creator import Dataset_creator
+import pandas as pd
+from dataset import Dataset
 from deal_with_categ_var import DealWithCategVar
-from deal_with_missing_values import DealWithMissingValues
-from imputer import Imputer
 
-dataframe_creator = Dataset_creator("train.csv", "test.csv", 'SalePrice')
+data_full_path_name = "debut.csv"
+working_set = pd.read_csv(data_full_path_name, index_col="Id")
+target = working_set.Prix
+working_set.drop(["Prix"], axis=1, inplace=True)
 
-# Extract the dataset into X_full, the target y and the test set X_test_full :
-X_full = dataframe_creator.get_dataset_full()
-y = dataframe_creator.get_dataset_target()
-X_test_full = dataframe_creator.get_dataset_test_full()
+dataset = Dataset(working_set, target)
 
-deal_with_categ_var = DealWithCategVar(X_full, 0)
-deal_with_categ_var.create_list_of_cols_with_categ_data_according_to_limiter()
-'''
-deal_with_missing_val_for_X = DealWithMissingValues(X, 'train', 0)
-X = deal_with_missing_val_for_X.drop_columns_with_missing_val_according_to_limiter()
-print(deal_with_missing_val_for_X.cols_with_missing_values_X)
-'''
+
+'''print(dataset.dataset)
+print(dataset.target)'''
