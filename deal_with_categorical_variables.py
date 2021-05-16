608c548c63676310bc144d80389eb54ba9775b1b
oxyde <s.gibard@gmail.com>
2021-05-16 15:58:38 +0200

    get unique entries per categorical columns implementation

--- deal_with_categorical_variables.py
+++ deal_with_categorical_variables.py
@@ -6,6 +6,7 @@ class DealWithCategoricalVariables:
     def __init__(self, working_set: pd.DataFrame, unique_var_limiter=None):
         self.working_set = working_set
         self.unique_var_limiter = unique_var_limiter
+        self.__get_unique_entries_per_categorical_columns()
 
         if unique_var_limiter is None:
             self.__get_categorical_columns()
@@ -25,5 +26,10 @@ class DealWithCategoricalVariables:
 
     def drop_categorical_columns(self):
         return self.working_set.drop(self.col_with_categ_data, axis=1)
+
+    def __get_unique_entries_per_categorical_columns(self):
+        self.unique_entries_per_categorical_columns = dict()
+        for col in self.working_set.columns:
+            self.unique_entries_per_categorical_columns[col] = len(self.working_set[col].unique())
 
 
